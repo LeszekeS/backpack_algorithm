ec1af33914599cecc00e5c07d06d4ee0e18fb872
LeszekeS <leszek.slupek@gmail.com>
2023-02-05 18:52:07 +0100

    added shop.txt file, added load method

--- thief_in_shop.py
+++ thief_in_shop.py
@@ -9,6 +9,8 @@ class Item():
         self.we_va_relation = value / weight
         Item.ekstensja.append(self)
 
+    def __str__(self):
+        return f'name: {self.name}; value: {self.value}'
 
 class Backpack():
 
@@ -16,3 +18,21 @@ class Backpack():
         self.capacity = capacity
         self.current_weight = 0
         self.items = list()
+
+
+def load_file(source):
+    file = open(source, 'r')
+    data = file.readlines()
+    file.close()
+
+    for row in data[1:]:
+        if row != "/n":
+            item = row.strip().split(';')
+            Item(item[0], float(item[1]), float(item[2]))
+
+
+load_file("shop.txt")
+
+print("items in shop")
+for i in Item.ekstensja:
+    print(i)
